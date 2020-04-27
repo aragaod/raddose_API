2e3b6a3e0262f3dfa9e1fdaa6cd9b8329b26d988
David Aragao <david.aragao@diamond.ac.uk>
2020-04-27 23:59:36 +0100

    Removing bug in find syntax on maintenance.sh file

--- maintenance.sh
+++ maintenance.sh
@@ -1,3 +1,3 @@
 du -hsc /run/user/1007182/raddose3d/
-find /run/user/1007182/raddose3d/* -mtime +3 -exec rm -rf {} \;
+find /run/user/1007182/raddose3d/ -mtime +3 -exec rm -rf {} \;
 du -hsc /run/user/1007182/raddose3d/
