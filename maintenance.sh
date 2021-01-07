7d206762acf11df38c4ae45df9ae4348223680d8
David Aragao <david.aragao@diamond.ac.uk>
2021-01-07 14:14:26 +0000

    Setting up cron job to clean ram disk and adding more info for logging purposes on cronjob running

--- maintenance.sh
+++ maintenance.sh
@@ -1,3 +1,8 @@
-du -hsc /run/user/1007182/raddose3d/
-find /run/user/1007182/raddose3d/ -mtime +3 -exec rm -rf {} \;
-du -hsc /run/user/1007182/raddose3d/
+echo "#########"
+/usr/bin/date
+/usr/bin/hostname
+echo "+++++++++"
+/usr/bin/du -hsc /run/user/1007182/raddose3d/
+/usr/bin/find /run/user/1007182/raddose3d/ -mtime +1 -exec rm -rf {} \;
+/usr/bin/du -hsc /run/user/1007182/raddose3d/
+echo "########"
