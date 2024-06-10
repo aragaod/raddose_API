2ca2c638c90b211fcf8a921bafdf9804167e49a5
David Aragao <david.aragao@diamond.ac.uk>
2024-06-10 12:08:51 +0100

    Changing back of run_dev to the development folder

--- run_dev.sh
+++ run_dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-cd /dls/science/groups/i04/Python/applications/raddoseAPI
+cd /dls/science/groups/i04/Python/applications/autodose
 /dls/science/groups/b21/PYTHON3/bin/uvicorn main:app --reload --host 0.0.0.0
