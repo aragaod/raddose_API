2dffdc480c874fd996ddc342d03cf0b235acdf5f
David Aragao <david.aragao@diamond.ac.uk>
2024-06-10 12:01:17 +0100

    Making more agressive deletions from RAM cache

--- maintenance_raddose_iXXcontrol.sh
+++ maintenance_raddose_iXXcontrol.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export uid=$(/usr/bin/id -u)
 
 echo "#########"
@@ -12,21 +14,21 @@ fi
 /usr/bin/du -hsc /run/user/${uid}/raddose3d/cache
 free_space=$(/usr/bin/df -k /run/user/${uid}/raddose3d/cache/ --output=pcent|tail -1|cut -d"%" -f1)
 echo "Free space: $free_space %"
-if (( ${free_space} < 10 ));then
+if (( ${free_space} < 5 ));then
     echo "Deleting less agressively days"
-    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mtime +2 -not -path "/run/user/1007182/raddose3d/cache" -exec rm -rf {} \;
-elif (( ${free_space} < 25 ));then
+    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mtime +2 -not -path "/run/user/${uid}/raddose3d/cache" -prune -exec rm -rf {} \;
+elif (( ${free_space} < 10 ));then
     echo "Deleting less agressively hours"
-    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +60 -not -path "/run/user/1007182/raddose3d/cache" -exec rm -rf {} \;
-elif (( ${free_space} < 50 ));then
+    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +50 -not -path "/run/user/${uid}/raddose3d/cache" -prune -exec rm -rf {} \;
+elif (( ${free_space} < 25 ));then
     echo "Deleting more agressively tens of minutes"
-    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +30 -not -path "/run/user/1007182/raddose3d/cache" -exec rm -rf {} \;
-elif (( ${free_space} < 75 ));then
+    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +15 -not -path "/run/user/${uid}/raddose3d/cache" -prune -exec rm -rf {} \;
+elif (( ${free_space} < 40 ));then
     echo "Deleting more agressively 5 minutes"
-    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +5 -not -path "/run/user/1007182/raddose3d/cache" -exec rm -rf {} \;
-elif (( ${free_space} < 90 ));then
+    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +3 -not -path "/run/user/${uid}/raddose3d/cache" -prune -exec rm -rf {} \;
+elif (( ${free_space} > 40 ));then
     echo "Deleting more agressively one minute"
-    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +1 -not -path "/run/user/1007182/raddose3d/cache" -exec rm -rf {} \;
+    /usr/bin/find /run/user/${uid}/raddose3d/cache -type d -mmin +1 -not -path "/run/user/${uid}/raddose3d/cache" -prune -exec rm -rf {} \;
 fi
 
 if  [ ! -d  /run/user/${uid}/raddose3d/cache/ ];then
